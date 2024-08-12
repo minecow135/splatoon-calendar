f8fb41545cea4994af2dded951e5c41850a808b4
mine_cow135 <40334322+minecow135@users.noreply.github.com>
2024-08-12 01:45:54 +0000

    changed run time to enviroment variable. moved run default to 06.00

--- src/splatfest/include.js
+++ src/splatfest/include.js
@@ -4,22 +4,30 @@ const createIcs = require('./createIcs.js');
 const discordSendNew = require('./discordSendNew.js');
 const discordSendWin = require('./discordSendWin.js');
 
+const run = Number(process.env.SPLATFEST_RUN_HOUR)
+const runFirst = Number(process.env.SPLATFEST_RUN_HOUR) - 1
+
 async function splatfest() {
     // Run once at the start
     getData();
     createIcs();
     discordSendNew();
     discordSendWin();
+    
+    if (!run || !runFirst) {
+        console.error("run not defined")
+    } else {
+        console.log('0 45 ' + runFirst + ' * * *')
+        cron.schedule('0 45 ' + runFirst + ' * * *', () => {
+            getData();
+        });
 
-    cron.schedule('0 45 1 * * *', () => {
-        getData();
-    });
-
-    cron.schedule('0 0 2 * * *', () => {
-        discordSendNew();
-        discordSendWin();
-        createIcs();
-    });
+        cron.schedule('0 0 ' + run + ' * * *', () => {
+            discordSendNew();
+            discordSendWin();
+            createIcs();
+        });
+    }
 };
 
 module.exports = splatfest;
